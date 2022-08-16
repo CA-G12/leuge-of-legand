177d390c3d71afe8b05d61500d147efd2fd8c84d
mohmmed23 <mhmmedben3@gmail.com>
2022-08-16 19:29:32 +0300

    3-create-landing page and connect with server side #3

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 
 const express = require('express')
 const app = express()
+const path=require('path')
 const router = require('./router')
 
 
 app.set('port', process.env.PORT || 3000); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  express.static(path.join(__dirname, '..', 'client'))
+);
 
 app.use(router)
 
