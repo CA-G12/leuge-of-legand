6f1d57ab820d465ef549b644e1eb1df60eb4e892
mohmmed23 <mhmmedben3@gmail.com>
2022-08-17 11:27:10 +0300

    edit style home page and handel data #15

--- client/dom.js
+++ client/dom.js
@@ -1,9 +1,11 @@
 const btn =document.querySelector('.search')
 const searchInput=document.querySelector('.search-input')
+const boxItems=document.querySelector('.box-items')
 fetch('/data')
     .then(res => res.json())
     .then((data) => {
-        /* console.log('Success:', data); */
+        createItems(Object.values(data.data))
+
     })
     .catch((error) => {
         console.error('Error:', error);
@@ -28,3 +30,27 @@ btn.addEventListener('click', (e) => {
             console.error('Error:', error);
         });
 })
+
+function createItems(data) {
+    data.forEach(champion => {
+        const boxItem=document.createElement('figure')
+        const div=document.createElement('div')
+        boxItems.appendChild(div)
+        div.appendChild(boxItem)
+        const imgItem=document.createElement('img')
+        imgItem.src='https://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/'+champion.image.full
+        const nameChampion=document.createElement('h3')
+        nameChampion.textContent=champion.name
+        boxItem.appendChild(imgItem)
+        boxItem.appendChild(nameChampion)
+
+        
+
+
+
+
+
+    });
+    console.log(data);
+    
+}
