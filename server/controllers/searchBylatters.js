ad177922e3183d4bbd62b65b5df858d68647ae98
mohmmed23 <mhmmedben3@gmail.com>
2022-08-17 15:55:00 +0300

    edit search bar #24

--- server/controllers/searchBylatters.js
+++ server/controllers/searchBylatters.js
@@ -3,6 +3,6 @@ const data = require('../names.json');
 const saerchByLitters = (req, res) => {
 // eslint-disable-next-line max-len
   const felteredData = data.filter((e) => e.toLowerCase().includes(req.body.name.toLowerCase().trim())).slice(0, 5);
-  res.json(JSON.stringify(felteredData));
+  res.json(felteredData);
 };
 module.exports = saerchByLitters;
